458bf0ac1c05cb47b34024006ed64515be1c42b2
Money-2021 <10fe82.80348a@gmail.com>
2021-11-09 09:09:28 +1000

    Optional JWToken Support

---
+++ SentinelVaultClient/Model/SecureIdentityRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentinelVaultClient.Model
+{
+    public class SecureIdentityRegistrationResponse
+    {
+        public string SecureIdentity { get; set; }
+        public string RecoveryToken { get; set; }
+        public string JWTToken { get; set; }
+        public byte[] HostECDHPublicKey { get; set; }
+        public byte[] HostECDSAPublicKey { get; set; }
+    }
+}
