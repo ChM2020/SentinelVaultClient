ab9fe17839f4384ec1ed233be05c6fec72e733f4
Money-2021 <10fe82.80348a@gmail.com>
2021-11-11 17:21:12 +1000

    JWToken mandatory for Release V.1.0.0

---
+++ SentinelVaultClient/Model/TransferOwnership.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentinelVaultClient.Model
+{
+    public class TransferOwnership
+    {
+        // Vaulted Object Identifier
+        public Guid ObjectId { get; set; }
+
+        // Existing Owner
+        public string OwnerSecureIdentity { get; set; }
+        public byte[] OwnerSignature { get; set; } // Signed (ObjectId | NewOwnerSecureIdentity)
+        public byte[] OwnerVaultKey { get; set; }  // Derived Owner Key
+
+        // New Owner
+        public string NewOwnerSecureIdentity { get; set; }
+        public byte[] NewOwnerEcdhPublicKey { get; set; } // Used to create new Owner VaultKey
+
+
+
+    }
+}
--- SentinelVaultClient/README.md
+++ SentinelVaultClient/README.md
@@ -1,4 +1,6 @@
 # SentinelVaultClient
  Sentinel Vault Service Client SDK
  Provides support for the Sentinel Vault Service..
- Contact your local Sentinel Provider for access details.
+ Contact your local Sentinel Provider for the issuance of a JWToken for API access details.
+ The Provider JWToken MUST be included within the "x-token" header field.
+
